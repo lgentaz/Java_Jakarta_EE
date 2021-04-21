a2e8dccf410d6788ec1147029fc5d521a2f1ba2c
lgentaz <lucile.gentaz@gmail.com>
2021-04-21 14:22:21 +0200

    script tomcat 9.0.45

--- .github/scripts/install_tomcat.sh
+++ .github/scripts/install_tomcat.sh
@@ -1,5 +1,5 @@
 set -eo
-curl https://downloads.apache.org/tomcat/tomcat-9/v9.0.5/bin/apache-tomcat-9.0.5.tar.gz -o /tmp/apache-tomcat-9.0.5.tar.gz
+curl https://downloads.apache.org/tomcat/tomcat-9/v9.0.5/bin/apache-tomcat-9.0.45.tar.gz -o /tmp/apache-tomcat-9.0.45.tar.gz
 mkdir /opt/tomcat
 tar xzvf /tmp/apache-tomcat*tar.gz -C /opt/tomcat --strip-components=1
 ls /opt/tomcat && ls /opt/tomcat/bin
